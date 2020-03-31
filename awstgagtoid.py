c2d246c50846cfbb4e2541948e58e75b01e9d750
Aminiasin <37815885+Aminiasin@users.noreply.github.com>
2020-03-31 17:28:47 +0200

    Update awstgagtoid.py

--- awstgagtoid.py
+++ awstgagtoid.py
@@ -1,5 +1,10 @@
 """
 @author: avale
+
+El siguiente codigo saca una lista con los ID de las isntancias EC2 que que tienen un tag concreto
+Tag =   key:value
+
+Incluye como autenticarse
 """
 
 import boto3
